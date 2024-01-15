510d383b79857578f58ef64c0e6bd1fa6f54fe83
Cifer0101 <l.kakabadze2020@gmail.com>
2024-01-15 17:39:16 +0400

    first commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 9,
+    text: "1 - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus corrupti sequi iure eius eligendi veritatis placeat dolores hic voluptatum mollitia.",
+  },
+  {
+    id: 2,
+    rating: 10,
+    text: "2 - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus corrupti sequi iure eius eligendi veritatis placeat dolores hic voluptatum mollitia.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "3 - Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus corrupti sequi iure eius eligendi veritatis placeat dolores hic voluptatum mollitia.",
+  },
+]
+
+export default FeedbackData
